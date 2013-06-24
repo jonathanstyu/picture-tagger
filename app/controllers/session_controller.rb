fd1c734d902ee8a5cd5a4eebb347c7eceb0ba155
App Academy Student <student@appacademy.io>
2013-06-24 16:52:09 -0700

    Added session login page

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,10 @@
+class SessionController < ApplicationController
+  def create
+    user = User.find_by_username(params[:username])
+    if user.verify_password(params[:password])
+      session[:token] = user.assign_new_token!
+    else
+      render :json => "Go away Hacker"
+    end
+  end
+end
